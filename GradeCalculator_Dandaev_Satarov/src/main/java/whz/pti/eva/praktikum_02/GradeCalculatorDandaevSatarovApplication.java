3e4e0ba93c1b7733b4b1ac57dc1c8d393ca49d18
[ald20kx6] <dandaev.alybek@gmail.com>
2020-10-20 00:14:04 +0200

    Erste Schritte beim Praktikum 2

--- GradeCalculator_Dandaev_Satarov/src/main/java/whz/pti/eva/praktikum_02/GradeCalculatorDandaevSatarovApplication.java
+++ GradeCalculator_Dandaev_Satarov/src/main/java/whz/pti/eva/praktikum_02/GradeCalculatorDandaevSatarovApplication.java
@@ -1,13 +1,19 @@
 package whz.pti.eva.praktikum_02;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GradeCalculatorDandaevSatarovApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeCalculatorDandaevSatarovApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner init() {
+		return (evt) -> System.out.println("Hello Welt!");
 	}
 
 }
