eae24df9c241701d4be62c46e16b68f93915b409
Bekzhan Satarov <satarovbekzhan@gmail.com>
2020-10-19 23:47:55 +0200

    Initial commit (Gradle Spring Project initialisiert)

---
+++ GradeCalculator_Dandaev_Satarov/src/main/java/whz/pti/eva/praktikum_02/GradeCalculatorDandaevSatarovApplication.java
@@ -0,0 +1,13 @@
+package whz.pti.eva.praktikum_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCalculatorDandaevSatarovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCalculatorDandaevSatarovApplication.class, args);
+	}
+
+}
---
+++ GradeCalculator_Dandaev_Satarov/src/test/java/whz/pti/eva/praktikum_02/GradeCalculatorDandaevSatarovApplicationTests.java
@@ -0,0 +1,13 @@
+package whz.pti.eva.praktikum_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCalculatorDandaevSatarovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
