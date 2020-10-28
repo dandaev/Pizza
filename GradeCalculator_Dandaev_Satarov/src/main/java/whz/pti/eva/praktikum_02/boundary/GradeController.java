0b720f947dbeb211705c96e2445be6646457fec6
Alybek Dandaev <dandaev.alybek@gmail.com>
2020-10-28 12:24:37 +0100

    Eingabe der neuen Noten realisiert

--- GradeCalculator_Dandaev_Satarov/src/main/java/whz/pti/eva/praktikum_02/boundary/GradeController.java
+++ GradeCalculator_Dandaev_Satarov/src/main/java/whz/pti/eva/praktikum_02/boundary/GradeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import whz.pti.eva.praktikum_02.domain.Grade;
 import whz.pti.eva.praktikum_02.service.GradeService;
@@ -21,5 +23,11 @@ public class GradeController {
 		List<Grade> grades = gradeService.listAllGrades();
 		model.addAttribute("grades", grades);
 		return "grades";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addGrade(@RequestParam String lecture,@RequestParam String grade) {
+		gradeService.addGrade(lecture, grade);
+		return "redirect:grades";
 	}
 }
