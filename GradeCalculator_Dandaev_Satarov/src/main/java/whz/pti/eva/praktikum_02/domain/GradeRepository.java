a4905305d130948dfa48afc1a940c40bade961d6
Alybek Dandaev <dandaev.alybek@gmail.com>
2021-05-28 02:55:04 +0200

    Deleted test project GradeCalculator

--- GradeCalculator_Dandaev_Satarov/src/main/java/whz/pti/eva/praktikum_02/domain/GradeRepository.java
+++
@@ -1,7 +0,0 @@
-package whz.pti.eva.praktikum_02.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GradeRepository extends JpaRepository<Grade, Integer> {
-	
-}
